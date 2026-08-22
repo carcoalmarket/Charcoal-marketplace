90adea670dcfdc339fa04472e4e2f3be3a9820e4
Charcoal Marketplace <charcoalmarketplace@gmail.com>
2026-08-22 17:21:30 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 const db = require("./config/db");
 
 const app = express();
-const PORT = Number(process.env.PORT || 5000);
+const PORT = Number(process.env.PORT || 8080);
 
 app.set("trust proxy", 1);
 
